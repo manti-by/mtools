5d4d48c5c156ec8fede53598837d7e87956e6af8
manti-by <manti.by@gmail.com>
2018-02-21 12:12:39 +0300

    Update gnome settings

--- fix_gnome.sh
+++ fix_gnome.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "Update system settings"
+gsettings set org.gnome.desktop.session idle-delay 900
+
 echo "Update nautilus settings"
 gsettings set org.gnome.nautilus.preferences default-sort-order type
 gsettings set org.gtk.Settings.FileChooser show-hidden false
