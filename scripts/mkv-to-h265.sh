cf59921741efcf5b4cbf9a90a9aa0968059cda99
manti-by <manti.by@gmail.com>
2018-07-26 17:00:17 +0300

    Update scripts

--- scripts/mkv-to-h265.sh
+++ scripts/mkv-to-h265.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-mkdir mkv
+mkdir h265
 for file in ./*.mkv
 do
     file_name="${file##*/}"   
     original_name="./"$file_name
-    result_name="./mkv/"${file_name%.*}".h265"
+    result_name="./h265/"${file_name%.*}".mkv"
     echo "Converting file: $original_name to $result_name"
     ffmpeg -i "$original_name" -c:v libx265 -crf 28 \
       -map 0:v -map 0:a:language:rus \
-      -vf "scale_npp=-1:720, scale_npp=trunc(iw/2)*2:720" \
-      -vcodec h264_nvenc -b:v 3000k -profile:v high \
-      -level 4.1 -preset medium \
+      -vf "scale=-1:720, scale=trunc(iw/2)*2:720" -b:v 4000k \
       -c:a libfdk_aac -b:a 128k -cutoff 18000 "$result_name"
 done
