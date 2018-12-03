22327f946a237fb98435593b6086fa6027f27015
manti-by <manti.by@gmail.com>
2018-12-03 12:59:50 +0300

    Add headers to update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
+CL="\033[0;96m"
+NC="\033[0m"
+
+function header () {
+    echo -e "${CL}$1"
+    echo -e "--------${NC}"
+}
+
+header "Update system packages"
 sudo apt update
 sudo apt upgrade -y
+
+header "Autoremove system packages"
 sudo apt clean 
 sudo apt autoremove --purge
+
+header "Update snaps"
+sudo snap refresh
