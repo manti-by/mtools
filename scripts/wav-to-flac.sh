d685276366078fa2c0143ef0ac2eacbaa643baec
manti-by <manti.by@gmail.com>
2019-11-27 10:39:21 +0300

    Update split tools, add ape and wav codecs

---
+++ scripts/wav-to-flac.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir flac
+for file in ./*.wav
+do
+    file_name="${file##*/}"   
+    original_name="./"$file_name
+    result_name="./flac/"${file_name%.*}".flac"
+    echo "Converting file: $original_name to $result_name"
+    ffmpeg -i "$original_name" -f flac "$result_name"
+done
